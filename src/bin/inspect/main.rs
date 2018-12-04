081cbc214a91fd37e7ea022a1fb9b0587b878b32
Matthias Endler <matthias-endler@gmx.net>
2018-12-04 19:33:51 +0100

    Better error messages

--- src/bin/inspect/main.rs
+++ src/bin/inspect/main.rs
@@ -13,11 +13,27 @@
 
 extern crate cargo_inspect;
 extern crate structopt;
+extern crate failure;
+
+use failure::Fail;
 
 use cargo_inspect::{config, inspect};
+use std::process;
 use structopt::StructOpt;
 
 fn main() {
     let config::Opt::Config(config) = config::Opt::from_args();
-    inspect(config).expect("Cannot print result")
+    if let Err(err) = inspect(config) {
+        eprintln!("Command failed:\n{}\n", err);
+
+        for cause in Fail::iter_causes(&err) {
+            println!("{}", cause);
+        }
+
+        if let Some(backtrace) = err.backtrace() {
+            eprintln!("Backtrace: {:?}", backtrace);
+        }
+
+        process::exit(1);
+    }
 }
