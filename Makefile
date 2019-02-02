9e8b51c96eae7af411ffae07a7650c75a5343c6b
Matthias Endler <matthias-endler@gmx.net>
2019-02-02 16:01:43 +0100

    Prototype

--- Makefile
+++ Makefile
@@ -5,6 +5,10 @@ SHELL := /bin/bash
 help: ## This help message
 	@echo -e "$$(grep -hE '^\S+:.*##' $(MAKEFILE_LIST) | sed -e 's/:.*##\s*/:/' -e 's/^\(.\+\):\(.*\)/\\x1b[36m\1\\x1b[m:\2/' | column -c2 -t -s :)"
 
+.PHONY: check
+check: ## Check project
+	cargo check
+
 .PHONY: build
 build: ## Build project
 	cargo build
--- src/diff.rs
+++ src/diff.rs
@@ -3,24 +3,59 @@ use std::path::PathBuf;
 pub use crate::errors::InspectError;
 pub use crate::tmpfile::write_tmp;
 
-/// Diff to input files and return the output
-/// We'll assume `git` is installed on the machine.
-/// We could also use a third-party crate for that,
-/// but the git diff output is reasonably stable and
-/// hassle-free.
-/// If git is not installed, this will return an error.
-pub fn diff(input1: String, input2: String) -> Result<String, InspectError> {
-    let file1: PathBuf = write_tmp(input1)?;
-    let file2: PathBuf = write_tmp(input2)?;
+use difference::{Difference, Changeset};
+use std::io::Write;
+use std::fs;
 
-    let output = Command::new("git")
-        .arg("diff")
-        .arg(file1)
-        .arg(file2)
-        .output()?;
-    let stderr = String::from_utf8(output.stderr)?;
-    if !stderr.is_empty() {
-        return Err(InspectError::Rustc(stderr));
+pub fn diff(text1: String, text2: String) -> Result<String, InspectError> {
+    let Changeset { diffs, .. } = Changeset::new(&text1, &text2, "\n");
+
+    let mut t = String::new();
+
+    for i in 0..diffs.len() {
+        match diffs[i] {
+            Difference::Same(ref x) => {
+                t.reset().unwrap();
+                writeln!(t, " {}", x);
+            }
+            Difference::Add(ref x) => {
+                match diffs[i - 1] {
+                    Difference::Rem(ref y) => {
+                        t.fg(term::color::GREEN).unwrap();
+                        write!(t, "+");
+                        let Changeset { diffs, .. } = Changeset::new(y, x, " ");
+                        for c in diffs {
+                            match c {
+                                Difference::Same(ref z) => {
+                                    t.fg(term::color::GREEN).unwrap();
+                                    write!(t, "{}", z);
+                                    write!(t, " ");
+                                }
+                                Difference::Add(ref z) => {
+                                    t.fg(term::color::WHITE).unwrap();
+                                    t.bg(term::color::GREEN).unwrap();
+                                    write!(t, "{}", z);
+                                    t.reset().unwrap();
+                                    write!(t, " ");
+                                }
+                                _ => (),
+                            }
+                        }
+                        writeln!(t, "");
+                    }
+                    _ => {
+                        t.fg(term::color::BRIGHT_GREEN).unwrap();
+                        writeln!(t, "+{}", x);
+                    }
+                };
+            }
+            Difference::Rem(ref x) => {
+                t.fg(term::color::RED).unwrap();
+                writeln!(t, "-{}", x);
+            }
+        }
     }
-    Ok(String::from_utf8(output.stdout)?)
+    // t.reset().unwrap();
+    // t.flush().unwrap();
+    Ok(t)
 }
