a0de208e581553349c44fe5ffa057001e69b6a81
Jonathan Steyfkens <jonathansty@users.noreply.github.com>
2019-02-05 12:33:32 +0100

    Adds a 'verbose' flag to enable/disable rustfmt error logging. (#21)

--- src/format.rs
+++ src/format.rs
@@ -3,15 +3,20 @@ use std::io::Write;
 use std::process::Command;
 use std::process::Stdio;
 
+use log::debug;
+
 // TODO: This should not call rustfmt from the commandline.
 // Instead, we should use it as a library. Oh well.
 pub fn format(input: &String) -> Result<String, InspectError> {
-    let mut cmd = Command::new("rustfmt")
+    let mut builder = Command::new("rustfmt");
+    builder
         .arg("--emit")
         .arg("stdout")
         .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .spawn()?;
+        .stderr(Stdio::piped())
+        .stdout(Stdio::piped());
+
+    let mut cmd = builder.spawn()?;
 
     cmd.stdin
         .as_mut()
@@ -19,5 +24,17 @@ pub fn format(input: &String) -> Result<String, InspectError> {
         .write_all(input.as_bytes())?;
 
     let output = cmd.wait_with_output().expect("Failed to read stdout");
+
+    // Only log formatting errors when `RUST_LOG=debug` is set. 
+    // If the formatting failed, we print out the plain text.
+    if !output.stderr.is_empty() {
+        debug!("Formatting failed with following errors:");
+        use std::io::BufRead;
+        for line in output.stderr.lines() {
+            let line = line?;
+            debug!("{}", line);
+        }
+    }
+
     Ok(String::from_utf8_lossy(&output.stdout).to_string())
 }
