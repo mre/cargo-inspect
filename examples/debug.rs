9d821ac69f8e67fc394aaa5b34f627cd86fd67f9
rafspiny <rafspiny@gmail.com>
2019-02-07 22:45:43 +0100

    Adding support for travis CI. Fix #24

--- examples/debug.rs
+++ examples/debug.rs
@@ -3,3 +3,13 @@ struct Point {
 	x: i32,
 	y: i32,
 }
+
+fn main() {
+	let a = Point {x: 10, y:10};
+	let b = Point {x: 5, y:16};
+
+	println!("a.x: {} a.y: {}", a.x, a.y);
+	println!("b.x: {} b.y: {}", b.x, b.y);
+
+    // TODO Do something more itneresting with these two refs
+}
